a44a716650b6d1a33e4e8568eb5913039967ffd3
Rayhchs <ray1515123@gmail.com>
2023-09-26 21:32:45 +0800

    canny edge

--- canny_edge/canny_edge.cpp
+++ canny_edge/canny_edge.cpp
@@ -8,10 +8,11 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #define PI 3.1415926
 
+enum dir{up, right, down, left, zero};
 
 void canny(cv::Mat &img) {
 
-	cv::Mat sobelx, sobely, intensity;
+	cv::Mat sobelx, sobely, sobelx_abs, sobely_abs, intensity;
 
 	// Gaussian Blur
 	cv::GaussianBlur(img, img, cv::Size(5,5),0);
@@ -19,18 +20,36 @@ void canny(cv::Mat &img) {
 	// Sobel Operation (Getting sobel intensity and angle)
  	cv::Sobel(img, sobelx, -1, 1, 0, 3);
 	cv::Sobel(img, sobely, -1, 0, 1, 3);
-	cv::convertScaleAbs(sobelx, sobelx);
-	cv::convertScaleAbs(sobely, sobely);
-	cv::addWeighted(sobelx, 0.5, sobely, 0.5, 0, intensity);
+	cv::convertScaleAbs(sobelx, sobelx_abs);
+	cv::convertScaleAbs(sobely, sobely_abs);
+	cv::addWeighted(sobelx_abs, 0.5, sobely_abs, 0.5, 0, intensity);
 
 	cv::Mat orientation(sobelx.size(), CV_32F);
 	for(int i=0; i<sobelx.rows; i++){
 		for(int j=0; j<sobelx.cols; j++){
-			// std::cout << std::atan(sobely.at<float>(i,j) / sobelx.at<float>(i,j)  * 180/ PI) << std::endl;
-			orientation.at<float>(i,j) = std::atan(sobely.at<float>(i,j) / sobelx.at<float>(i,j)) * 180 / PI;
-			std::cout << orientation.at<float>(i,j) << " ";
+			float x = (sobelx.at<float>(i,j) != 0) ? sobelx.at<float>(i,j) : 10e-5;
+			float angle = std::atan(sobely.at<float>(i,j) / x) * (180 / PI);
+			int a = angle;
+			if ((angle <=22.5 && angle >=0) || (angle >=157.5 && angle <=180)){
+				orientation.at<int>(i,j) = up;
+			}
+			else if ((angle <22.5 && angle >=67.5)){
+				orientation.at<int>(i,j) = right;
+			}
+			else if ((angle <67.5 && angle >=112.5)){
+				orientation.at<int>(i,j) = down;
+			}
+			else if ((angle <112.5 && angle >=157.5)){
+				orientation.at<int>(i,j) = down;
+			}
+			else{
+				orientation.at<int>(i,j) = zero;
+			}
+			// orientation.at<float>(i,j) = std::atan(sobely.at<float>(i,j) / x) * (180 / PI);
+			std::cout << orientation.at<int>(i,j) << " " << a << ";";
 		}
 	}
+
 	std::cout << std::endl;
 }
 
@@ -43,6 +62,5 @@ int main(){
 	canny(grayImage);
 
 	// cv::imwrite("sobel_wo_cv.jpg", sobel_wo_cv);
-
 	return 0;
 }
